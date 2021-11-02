5c60df6c9705872266dbd488f9b47516a9e0a116
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2021-11-02 20:15:19 +0700

    Update TxtFile.cpp

--- DoAnHDH/TxtFile.cpp
+++ DoAnHDH/TxtFile.cpp
@@ -1,1 +1,20 @@
+#pragma once
 #include "TxtFile.h"
+#include "FAT32.h"
+#include "ReadData.h"
+
+void printTxtFile(vector<TxtFile> Files)
+{
+	string FileName;
+	byte* FileData;
+	int FileSize;
+	for (int i = 0; i < Files.size(); i++)
+	{
+		FileName = Files.at(i).name;
+		FileData = Files.at(i).data;
+		FileSize = Files.at(i).size;
+		cout << "FileName: " << FileName << endl;
+		cout << ReadtoString(FileData, "0",FileSize);
+		cout << "\n=====================\n";
+	}
+}
