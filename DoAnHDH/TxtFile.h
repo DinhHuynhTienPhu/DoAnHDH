deeb73babb7c88d50b055d4fe08cfb9635c0fc43
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2021-11-02 20:14:49 +0700

    Update TxtFile.h

--- DoAnHDH/TxtFile.h
+++ DoAnHDH/TxtFile.h
@@ -1,10 +1,14 @@
-﻿#pragma once
+#pragma warning(disable:4996)
+#pragma once
 #include <string>
 #include <Windows.h>
+#include<vector>
 using namespace std;
 struct TxtFile
 {
 	string name;
-	BYTE* data;
+	BYTE * data;
+	int size;
 	// nếu được thì xuất luôn có dấu
 };
+void  printTxtFile(vector<TxtFile> Files);
