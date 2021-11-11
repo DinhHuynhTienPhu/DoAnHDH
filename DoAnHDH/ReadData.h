0a3d1430e07715ddc7552463f0d5c1cb3e6bb4a9
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2021-11-12 00:05:30 +0700

    Khôi phục ver cũ

--- DoAnHDH/ReadData.h
+++ DoAnHDH/ReadData.h
@@ -5,21 +5,11 @@
 #include<sstream>
 #include <vector>
 #include <Windows.h>
-#include<conio.h>
-#include <io.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <algorithm>
 
 int ReadData(LPCWSTR drive, int readPoint, BYTE* sector, int bytes);// thêm tham số bytes cần đọc -> đây không còn là hàm đọc sector nên t đổi tên
-
 uint64_t ReadIntReverse(uint8_t* byte, std::string offsetHex, unsigned int count);
-
 int hexCharToInt(char a);
-
 std::string hexToString(std::string str);
-
 std::string ReadtoString(BYTE* data, std::string offsetHex, unsigned int bytes);
-
 void printTextData(byte* data, int size);//xuất text từ byte*, nếu dùng hàm ReadtoString để in file text, tốc độ rất chậm
 std::wstring s2ws(const std::string& s);
