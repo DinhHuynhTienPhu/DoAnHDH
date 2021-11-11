f87f7cf34f404370cc3bb070af57e4a8eef3a8ab
nqan1409 <quangtrungnguyen05021973@gmail.com>
2021-11-12 00:31:42 +0700

    update NTFS

--- DoAnHDH/NTFS.cpp
+++ DoAnHDH/NTFS.cpp
@@ -1,7 +1,4 @@
 #include "NTFS.h"
-#include <algorithm>
-#include "ReadData.h"
-#include "TxtFile.h"
 
 void NTFS::read(BYTE* sector)
 {
@@ -12,6 +9,7 @@ void NTFS::read(BYTE* sector)
 	sectorStart = ReadIntReverse(sector, "1C", 4); //offset 1C - 4 bytes
 	driveSize = ReadIntReverse(sector, "28", 8); //offset 20 - 4 bytes
 	startCluster = ReadIntReverse(sector, "30", 8); //offset 30 - 8 bytes
+	mtfSize = pow(2.0, abs(converter(sector, "40", 1)));//offset 40 - 1 byte
 	sectorsPerIndexBuffer = ReadIntReverse(sector, "44", 1); //offset 44 - 1 byte
 }
 
@@ -24,5 +22,78 @@ void NTFS::print() const
 	cout << "+ Sector bat dau cua o dia logic: " << sectorStart << " sectors" << endl;
 	cout << "+ So sector cua o dia: " << driveSize << " sectors" << endl;
 	cout << "+ Cluster bat dau cua MFT: " << startCluster << endl;
+	cout << "+ Kich thuoc cua MFT: " << mtfSize << " bytes" << endl;
 	cout << "+ So cluster cua Index Buffer: " << sectorsPerIndexBuffer << endl;
+}
+
+string getBit(int x) {
+	string bit, bits;
+
+	for (int i = 0; i < 4; i++)
+	{
+		bit = 48 + ((x >> (3 - i)) & 1);
+		if (bit == "1") {
+			bit = "0";
+		}
+		else {
+			bit = "1";
+		}
+		bits.append(bit);
+	}
+
+	return bits;
+}
+
+void getNotBit(string bit) {
+	for (int i = 0; i < bit.size(); i++)
+	{
+
+	}
+}
+
+int bitToInt(string bit) {
+	int result = 0;
+
+	for (int i = 1; i < bit.size(); i++)
+	{
+		result += (bit[i] - 48) * (pow(2.0, (7.0 - i)));
+	}
+
+	return result;
+}
+
+string hexStr(BYTE* data, int len)
+{
+	std::stringstream ss;
+	ss << std::hex;
+
+	for (int i(0); i < len; ++i)
+		ss << std::setw(2) << std::setfill('0') << (int)data[i];
+
+	return ss.str();
+}
+
+int converter(uint8_t* byte, string offsetHex, int len)
+{
+	const int offset = stoi(offsetHex, nullptr, 16);
+
+	byte += offset;
+	string s = hexStr(byte, len);
+	int a = 0;
+	int x = hexCharToInt(s[0]);
+	int y = hexCharToInt(s[1]);
+	string temp;
+	string bit;
+	temp = getBit(x);
+	bit.append(getBit(x));
+	bit.append(getBit(y));
+
+	if (bit[0] == '0') {
+		a = (bitToInt(bit) + 1) * -1;
+	}
+	else {
+		a = bitToInt(bit) + 1;
+	}
+
+	return a;
 }
--- DoAnHDH/NTFS.h
+++ DoAnHDH/NTFS.h
@@ -3,9 +3,13 @@
 #include <Windows.h>
 #include <string>
 #include <vector>
-
+#include <iomanip>
+#include <math.h>
+#include <algorithm>
+#include "ReadData.h"
 #include "TxtFile.h"
 using namespace std;
+
 struct NTFS
 {
 	LPCWSTR drive;
@@ -16,7 +20,18 @@ struct NTFS
 	int sectorStart; //offset 1C - 4 bytes
 	int driveSize; //offset 28 - 8 bytes
 	int startCluster; //offset 30 - 8 bytes
+	int mtfSize; //offset 40 - 1 byte
 	int sectorsPerIndexBuffer; //offset 44 - 1 byte
 	void read(BYTE* sector);
 	void print() const;
 };
+
+string getBit(int x);
+
+void getNotBit(string bit);
+
+int bitToInt(string bit);
+
+string hexStr(BYTE* data, int len);
+
+int converter(uint8_t* byte, string offsetHex, int len);
