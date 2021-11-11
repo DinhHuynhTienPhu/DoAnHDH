8355eff1e4a1cf35c41addf812415dfeecd74385
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2021-11-11 19:57:08 +0700

    Update FAT32.h

--- DoAnHDH/FAT32.h
+++ DoAnHDH/FAT32.h
@@ -1,8 +1,12 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
 #include <Windows.h>
 #include <string>
 #include <vector>
+#include <fcntl.h>
+#include<iostream>
+
+
 
 #include "TxtFile.h"
 using namespace std;
@@ -33,14 +37,12 @@ vector<int> clusterArray(FAT32 volume, int startCluster);
 
 vector<bool> ConvertByteToBoolArray(byte b);
 
-vector<byte> ReadRawByte(int start, int length, vector<byte> data)//t doi ten vi o duoi t co xai de tach byte ra
-;
-
+vector<byte> ReadRawByte(int start, int length, vector<byte> data);//t doi ten vi o duoi t co xai de tach byte ra
 
 void printInfoOfMainEntry(vector< BYTE> e);
 
 
 void ReadEntries(int start, int tab, vector<BYTE> det, bool isRdet,FAT32 volume,vector<TxtFile> &txtFiles);
 
-void folderHandler(string fileName, vector< byte> entry, int tab, FAT32 volume, vector< TxtFile> &txtFiles);
-void fileHandler(string fileName, string extension, vector<byte> entry, int tab, FAT32 volume, vector<TxtFile> &txtFiles);
+void folderHandler(wstring fileName, vector< byte> entry, int tab, FAT32 volume, vector< TxtFile> &txtFiles);
+void fileHandler(wstring fileName, wstring extension, vector<byte> entry, int tab, FAT32 volume, vector<TxtFile> &txtFiles);
