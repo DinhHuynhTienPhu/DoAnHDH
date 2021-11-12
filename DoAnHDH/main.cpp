ba1667e17fea80237f8c364d0be5d7872ede8a3a
DinhHuynhTienPhu <ff101111011@gmail.com>
2021-11-13 00:01:18 +0700

    Update main.cpp

--- DoAnHDH/main.cpp
+++ DoAnHDH/main.cpp
@@ -23,7 +23,6 @@ int main(int argc, char** argv)
 	LPCWSTR drive = stemp.c_str();
 	ReadData(drive, 0, bootSector, 512);// ổ đĩa cần đọc, offset đọc, buffer, số byte đọc
 	volume.drive = drive;//lưu tên ổ đĩa lại vào volume, nếu cần dùng đến thì vào thuộc tính drive trong struct này
-
 	string type = ReadtoString(bootSector, "52", 5);//check có phải fat32 không
 	if (type == "FAT32") {
 		volume.read(bootSector);
@@ -44,9 +43,9 @@ int main(int argc, char** argv)
 		cout << "\nDay la NTFS\n";
 		BYTE bootSectorNTFS[512];
 		NTFS volume;
-		volume.drive = drive;//lưu tên ổ đĩa lại vào volume, nếu cần dùng đến thì vào thuộc tính drive trong struct này
+		ReadData(drive, 0, bootSectorNTFS, 512);// ổ đĩa cần đọc, offset đọc, buffer, số byte đọc
 		volume.read(bootSectorNTFS);
 		volume.print();
 	}
-	else cout << "\nDa co loi, khong the doc. Xin kiem tra lai";
+	else cout << "\nDay khong phai FAT32 hay NTFS. Xin kiem tra lai";
 }
